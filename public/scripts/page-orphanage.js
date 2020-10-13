44fde5194b231131b6e7a3641c03f4c36c2e81a3
Bruna Delmouro <brunadelmouro@gmail.com>
2020-10-13 19:47:40 -0300

    ep-02

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,56 @@
+//objects
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrolWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([-22.2768479,-46.3724058], 15) //ouro fino MG
+
+//create and add titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L
+.marker([-22.2768479,-46.3724058], { icon })
+.addTo(map)
+
+// image galry
+
+function selectImage(event){
+    const button = event.currentTarget
+
+    //remover  todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove('active');
+    }
+
+    //selecionar a 'image' clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o  container de 'image'
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botão
+    button.classList.add('active')
+
+
+}
+
